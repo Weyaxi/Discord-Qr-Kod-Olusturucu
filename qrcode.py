2b9b6a0cf28cd641a265623d4e861977c0132d2b
Weyaxi <81961593+Weyaxi@users.noreply.github.com>
2021-05-11 16:59:09 +0300

    Update qrcode.py

--- qrcode.py
+++ qrcode.py
@@ -13,6 +13,7 @@ async def on_ready():
     print(f'{bot.user.name} Olarak Giriş Yapıldı')
     print(f'Discord Versiyonu {discord.__version__}')
     print('----------------------------')
+    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.playing, name=f"qr!karekod"))
 
 
 @bot.command(pass_context=True)
